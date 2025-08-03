9b104341ef0028ae34ccca0a93020f7c5e806cf9
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2025-08-03 12:55:17 +0800

    feat: Implement workflow

---
+++ scripts/metadata.py
@@ -0,0 +1,44 @@
+import unzip_http
+from sys import argv
+from os import environ
+
+def setOutput(key, value):
+    """Set the output for GitHub Actions."""
+    output = environ.get("GITHUB_OUTPUT")
+    if output:
+        with open(environ["GITHUB_OUTPUT"], "a") as f:
+            f.write(f"{key}={value}\n")
+    print(f"setOutput: {key}={value}") # Print the output
+
+
+# Get medadata
+url = argv[1]
+print("Extracting metadata from:", url)
+rzf = unzip_http.RemoteZipFile(url)
+metadata_text = rzf.open_text('META-INF/com/android/metadata').read()
+print("META-INF/com/android/metadata:")
+print(metadata_text)
+
+# Parse metadata
+metadata = {}
+for line in metadata_text.splitlines():
+    key, value = line.split("=", maxsplit=1)
+    if key and value:
+        metadata[key.strip()] = value.strip()
+print("Parsed metadata:")
+print(metadata)
+
+# Generate release note
+metadata_md = ""
+for k, v in metadata.items():
+    metadata_md += f"- `{k}`: {v}\n"
+
+note = f"""## Metadata
+
+{metadata_md}"""
+
+with open("output/note.md", "w") as f:
+    f.write(note)
+
+# Set output for later use
+setOutput("version_name", metadata["version_name"])
